fa2882dbafaa5898e5919cfb8449599b614e71db
Mainu0177 <mainuddinhassan658@gmail.com>
2025-01-02 12:44:41 +0600

    create product details

--- frontend/src/pages/ProductDetail.js
+++ frontend/src/pages/ProductDetail.js
@@ -1,9 +1,50 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+
+import SummeryApi from '../common/urlIntigration'
 
 const ProductDetail = () => {
+  const [data, setData] = useState({
+    productName : "",
+    brandName : "",
+    category : "",
+    productImage : [],
+    description: "",
+    price : "",
+    sellingPrice : "",
+  })
+  const [loading, setLoading] = useState(false)
+
+  const params = useParams();
+
+  console.log("product id", params)
+
+  const fetchProductDetails = async () =>{
+    setLoading(true)
+    const response = await fetch(SummeryApi.productDetails.url, {
+      method : SummeryApi.productDetails.method,
+      headers : {
+        "Content-Type" : "application/jsonh"
+      },
+      body : JSON.stringify({
+        productId: params?.id
+      })
+    })
+    setLoading(false)
+    const dataResponse = await response.json()
+
+    setData(dataResponse?.data)
+  }
+
+  console.log("data", data)
+  useEffect(() =>{
+    fetchProductDetails()
+  }, [])
   return (
     <div>ProductDetail</div>
   )
 }
 
 export default ProductDetail
+
+
